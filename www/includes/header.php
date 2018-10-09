352391e1d8be794e52bdbcfd6f3111697524a0e3
Daniel Haehn <d@ey-i.org>
2018-10-09 19:22:30 -0400

    Add new style.

--- www/includes/header.php
+++ www/includes/header.php
@@ -1,8 +1,10 @@
 <html>
   <head>
-     <title>RateMyTalk.com</title>
+    <title>RateMyTalk.com</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
   </head>
   <body>
+    <div id="logo"><img src="gfx/logo.png"></div>
 
 
 
