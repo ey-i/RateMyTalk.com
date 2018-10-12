8c9e7901258e1f61c6b00ee03f349c120f3a062c
Daniel Haehn <d@ey-i.org>
2018-10-12 13:20:25 -0400

    Add DB access.

--- www/content/create.php
+++ www/content/create.php
@@ -20,8 +20,29 @@
 
   // create new DB entry
   $new_id = getToken(6);
-  
 
+  $new_id = "E4SZSM";
+
+  $db = new mysqli("localhost", "", "", "ratemytalk");
+  if ($db->connect_error) {
+    die("DB error (1).");
+  }
+
+  $sql = "INSERT INTO talk (id, email, title) VALUES (?,?,?)";
+  $sql = $db->prepare($sql);
+  $sql->bind_param("sss", $new_id, $email, $title);
+  $status = $sql->execute();
+
+  if ($status) {
+    echo "OK";
+  } else {
+    die("DB error (2).");
+  }
+
+  echo $new_id;
+
+  $sql->close();
+  $db->close();
 
   // show DB entry
 
