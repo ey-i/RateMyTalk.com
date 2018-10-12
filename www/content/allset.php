12815e34b16be5b68aded0839145901cba3bfb14
Daniel Haehn <haehn@seas.harvard.edu>
2018-10-12 15:23:23 -0400

    All set page ready.

---
+++ www/content/allset.php
@@ -0,0 +1,13 @@
+
+<div id="welcome">
+
+  You are all set.
+
+  <br><br>
+
+  Please share the following link with your audience.
+
+  <br><br>
+  <a href="http://ratemytalk.com/<?php echo $new_id; ?>" target="_blank">RateMyTalk.com/<?php echo $new_id; ?></a>
+
+</div>
